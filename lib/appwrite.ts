89d376c0b5650fc6fef982f465f3c4a381bb997c
Khalid Redwan Sun <khalid.redwan.sun701@gmail.com>
2025-12-31 02:18:12 +0600

    Add Appwrite configuration and dependencies; create .env file for project settings

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1", // Your Appwrite Endpoint
+  platform: "com.khalidFood.fastFood",
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
+};
