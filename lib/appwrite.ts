38a119b8771c5d34e2b0f8d4acd463702701390c
Khalid Redwan Sun <khalid.redwan.sun701@gmail.com>
2026-01-01 19:44:20 +0600

    Refactor authentication screens and layout; implement custom input and button components

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,7 @@
 export const appwriteConfig = {
-  endpoint:
-    process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.khalidFood.fastFoodApp",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "69555d28003463c1d1b8",
+  userCollectionId: "user",
 };
