ea90de305cbcdecf0faf327b4bf9bf252a014e6d
Khalid Redwan Sun <khalid.redwan.sun701@gmail.com>
2026-01-03 23:25:53 +0600

    Integrate Sentry for error tracking and monitoring; update dependencies and configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,28 @@ import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./global.css";
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://e839208cdfd063572261cde11b5eae71@o4510493799284736.ingest.us.sentry.io/4510647450599424',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
@@ -23,4 +43,4 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaProvider>
   );
-}
+});
